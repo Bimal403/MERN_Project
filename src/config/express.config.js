86629d0c8e58c6fb26598ce25eba259468e1cd89
BimalTimsina <bimaltimsina4@gmail.com>
2023-10-03 23:50:00 +0545

    day 3 more exploration on routing and middleware

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -3,4 +3,14 @@ const app = express();
 app.get("/", (request, response) => {
   response.send("hi");
 });
+app.get("/about", (req, res) => {
+  res.json({
+    meta: null,
+    msg: "hi",
+  });
+});
+app.get("/user/:userId", (req, res) => {
+  let userId = req.params.userId;
+  res.send(userId);
+});
 module.exports = app;
